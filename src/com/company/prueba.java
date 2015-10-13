45b6430e1e12f9137584af9c51f5fa5d203e09b4
Yamir Gudiño <gyamir12@gmail.com>
2015-10-12 22:53:45 -0500

    prueba

---
+++ src/com/company/prueba.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by yamir on 10/12/15.
+ */
+public class prueba {
+    public static void main(String[] args){
+        System.out.println("esto es una prueba");
+    }
+}
